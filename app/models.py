34dd9b215e98bc2138a82644bcac748f2d4dfd92
VligendeHollander <DevOps@cefimmac91.home>
2019-11-19 22:58:34 +0100

    Create models

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(45), index=True, unique=True, nullable=False)
+    lastname = db.Column(db.String(45), index=True, unique=True, nullable=False)
+    username = db.Column(db.String(45), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(255), index=True, nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)    
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
