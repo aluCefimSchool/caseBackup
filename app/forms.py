1a195f9685f71b9242f8cd1c6c9a7e514565a2ba
VligendeHollander <DevOps@cefimmac91.pedago>
2019-11-19 09:32:30 +0100

    Create signup form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField, DateField
 import os
 
 class SignInForm(FlaskForm):
@@ -8,3 +10,18 @@ class SignInForm(FlaskForm):
     # recaptcha = RecaptchaField()
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Se connecter')
+
+class SignUpForm(FlaskForm):
+    lastname = StringField('Nom', [validators.DataRequired()])
+    firstname = StringField('Prénom', [validators.DataRequired()])
+    username = StringField('Nom d\'utilisateur', [validators.DataRequired()])
+    email = EmailField('Email', [validators.DataRequired()])
+    password = PasswordField('Mot de passe', [validators.DataRequired()])
+    confirmPass = PasswordField('Confimer le mot de passe', [
+        validators.DataRequired(), 
+        validators.EqualTo('password', 'La donnée saisie ne correspond pas à votre mot de passe !')
+    ])
+    # birthday = DateField('Date de naissance', [validators.DataRequired()])
+    # accept_tos = BooleanField('Termes d\'Utilisation', [validators.DataRequired()])
+    # recaptcha = RecaptchaField()
+    submit = SubmitField('S\'enregistrer')
