4d27ed6eb2b2dfa2efaeffa40866cfac2ef35a95
VligendeHollander <DevOps@cefimmac91.home>
2019-11-19 22:57:38 +0100

    Import sqlalchemy and flask_login to app

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@
 ##
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 #Predefined variable to configure Flask
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
 
-from app import routes
+from app import routes, models
