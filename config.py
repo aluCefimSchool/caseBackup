3f06aef64cbe399aeac01409abbec378fd0630f4
VligendeHollander <DevOps@cefimmac91.pedago>
2019-11-19 14:11:34 +0100

    Add httponly in flask configuration

--- config.py
+++ config.py
@@ -6,4 +6,8 @@ class Config(object):
     RECAPTCHA_USE_SSL = False
     RECAPTCHA_PRIVATE_KEY = os.environ.get('CAPTCHA_PRIVATE_KEY') 
     RECAPTCHA_PUBLIC_KEY = os.environ.get('CAPTCHA_PUBLIC_KEY')
+
+    #COOKIE / XSS
+    SESSION_COOKIE_HTTPONLY = True
+    REMEMBER_COOKIE_HTTPONLY = True
     
