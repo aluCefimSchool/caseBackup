1989cf8cd15df4b38fd50a2c73a1d7905ba7a44f
VligendeHollander <DevOps@cefimmac91.local>
2019-11-20 10:22:54 +0100

    Update route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, flash, redirect, request, session, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required, logout_user
 
 from app import app 
+from app import db
 from app.forms import SignInForm, SignUpForm
 from app.models import User
 
@@ -16,27 +17,42 @@ def index():
     #IF USER CONNECTED REDIRECT TO DASHBOARD
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
-    #Send form if condictions = OK and redirect
+    
+    #SEND FROM IF CONDITIONS = OK AND REDIRECTS
     if request.method == 'POST' and form.validate_on_submit():
-        session.permanent = True
-        user = request.form
-        session["user"] = user
-        flash('Bienvenue à vous {}, remember_me={}'.format(form.username.data, form.remember_me.data, "info"))
-        return redirect(url_for('dashboard'))
 
-        found_user = users.query.filter_by(name = user).first()
-        if found_user:
-            session["email"] = found_user.email
-        else:
-            usr = users(user, "")
-            db.session.add(usr)
-            db.session.commit()
-
-    else:
-        if "user" in session: 
-            return redirect(url_for('dashboard'))
+        #VERIFY IF USER EXIST IN DATABASE
+        user = User.query.filter_by(username=form.username.data).first()
         
-        return render_template('signin.html', title='Sign In', form=form)
+        #IF USER OR PASSWORD DONT MATCH RETURN TO LOGIN
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('index'))
+        
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('dashboard'))
+    
+    return render_template('signin.html', title='Sign In', form=form)
+
+
+    #     session.permanent = True
+    #     user = request.form
+    #     session["user"] = user
+    #     flash('Bienvenue à vous {}, remember_me={}'.format(form.username.data, form.remember_me.data, "info"))
+    #     return redirect(url_for('dashboard'))
+
+    #     found_user = users.query.filter_by(name = user).first()
+    #     if found_user:
+    #         session["email"] = found_user.email
+    #     else:
+    #         usr = users(user, "")
+    #         db.session.add(usr)
+    #         db.session.commit()
+
+    # else:
+    #     if "user" in session: 
+    #         return redirect(url_for('dashboard'))
+        
    
 
 ##
@@ -45,40 +61,40 @@ def index():
 @app.route('/signup', methods=['GET', 'POST'])
 def signUp():
     form = SignUpForm()
+    
+    if current_user.is_authenticated:
+        return redirect(url_for('dashboard'))
+        flash(f"Vous êtes déja inscrit sur notre application !", "info")
 
     #Send form if conditions = OK and redirect
     if request.method == 'POST' and form.validate_on_submit():
+        user = User(lastname = form.lastname.data, firstname = form.firstname.data, username=form.username.data, email=form.email.data, birthday=form.birthday.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
         flash('Votre inscription a été prit en compte {}. Veuillez vérifier vos email pour confirmer votre inscription'.format(form.username.data, "info"))
         return redirect(url_for('index'))
-    else: 
-        if "user" in session:
-            flash(f"Vous êtes déja inscrit sur notre application !", "info")
-            return redirect(url_for("dashboard"))
     
-    return render_template('signup.html', title='Sign Up', form=form)
+    return render_template('signup.html', title='Sign Up', form=form)    
 
 ##
 # LOGOUT ROUTE
 ##
 @app.route('/logout')
 def logOut():
-    if "user" in session:
-        user = session["user"]
-        flash(f"Vous avez été déconnecté de l'application !", "info")
-    
-    session.pop("user", None)
-    return redirect(url_for("index"))
+    logout_user()
+    flash(f"Vous avez été déconnecté de l'application !", "info")
+    return redirect(url_for('index'))
 
 ##
 # DASHBOARD ROUTE
 ##
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    if "user" in session:
-        user = session["user"]
-
+    if current_user.is_authenticated:
         #Return template index.html with data
-        return render_template('dashboard.html', title='Dashboard', user=user)
+        return render_template('dashboard.html', title='Dashboard')
     
     else:
         #Redirect to signin route
@@ -88,6 +104,7 @@ def dashboard():
 # PROFIL ROUTE
 ##
 @app.route('/profil')
+@login_required
 def profil():
     if "user" in session:
         user = session["user"]
