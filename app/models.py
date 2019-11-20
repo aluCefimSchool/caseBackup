f0750bb004bf8eb90fd47b5b483dacd6a04ef0cb
VligendeHollander <DevOps@cefimmac91.local>
2019-11-20 10:22:16 +0100

    Update fields models

--- app/models.py
+++ app/models.py
@@ -1,26 +1,28 @@
 from app import db
 from app import login
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(45), index=True, unique=True, nullable=False)
     lastname = db.Column(db.String(45), index=True, unique=True, nullable=False)
     username = db.Column(db.String(45), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password = db.Column(db.String(255), index=True, nullable=False)
+    birthday = db.Column(db.Date, index=True, nullable=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
 
