f38fd52d3b6a0b6d22d050336372305d9a2b4d70
VligendeHollander <DevOps@cefimmac91.local>
2019-11-20 10:21:18 +0100

    Add protect route

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 ##
-# Initialisation Flask
+# INITIALISATION FLASK
 ##
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-#Predefined variable to configure Flask
 app = Flask(__name__)
+
+#IMPORT CONFIGURATION FLASK
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
+
 login = LoginManager(app)
+
+#PROTECT ROUTE AGAINST ANONYMOUS USERS / @login_required
+login.login_view = 'login'
 
 from app import routes, models
